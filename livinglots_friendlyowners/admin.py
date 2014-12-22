09ed9b31ffbf0e3e6959c4fd598e98eb88d1b1ab
Eric Brelsford <ebrelsford@gmail.com>
2014-12-22 12:00:02 -0500

    Lot readonly too

--- livinglots_friendlyowners/admin.py
+++ livinglots_friendlyowners/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 class FriendlyOwnerAdminMixin(admin.ModelAdmin):
     fields = ('name', 'email', 'phone', 'notes', 'parcels', 'lot', 'added',)
     list_display = ('name', 'email', 'phone', 'lot', 'added',)
-    readonly_fields = ('added', 'parcels',)
+    readonly_fields = ('added', 'lot', 'parcels',)
